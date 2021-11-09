92719ac24adfd922105f7db2ffa4a1201dbad98a
mystics cove <mysticscove@mysticss-MacBook-Pro.local>
2021-11-09 04:36:52 -0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,304 @@
+
+enum STATES { standby, game, gameOver }
+enum TetriminoTypes { I, J, L, O, S, T, Z }
+
+type Point = { x : number, y : number }
+type Tetrimino =
+    { x : number
+    , y : number
+    , type : TetriminoTypes
+    , points : Point[]
+    }
+
+const canvas = document.getElementById('canvas')! as HTMLCanvasElement
+const ctx = canvas.getContext('2d')!
+const W = 10
+const H = 20
+const TICK = 250
+const PRESSING = { none: 0, left: 1, up: 2, right: 4, down: 8, 'rotate-left': 16, 'rotate-right': 32 } as const
+const KeyMap = { a: 'left', w: 'up', d: 'right', s: 'down', q: 'rotate-left', e: 'rotate-right' } as const
+const TetriminoShapes : Record<TetriminoTypes, Point[]> = 
+    { [TetriminoTypes.I]: [[-1,0],[0,0],[1,0],[2,0]].map(([x,y]) => ({ x, y }))
+    , [TetriminoTypes.J]: [[-1,0],[0,0],[1,0],[-1,1]].map(([x,y]) => ({ x, y }))
+    , [TetriminoTypes.L]: [[-1,0],[0,0],[1,0],[1,1]].map(([x,y]) => ({ x, y }))
+    , [TetriminoTypes.O]: [[0,0],[1,0],[0,1],[1,1]].map(([x,y]) => ({ x, y }))
+    , [TetriminoTypes.S]: [[-1,0],[0,0],[0,1],[1,1]].map(([x,y]) => ({ x, y }))
+    , [TetriminoTypes.T]: [[-1,0],[0,0],[0,1],[1,0]].map(([x,y]) => ({ x, y }))
+    , [TetriminoTypes.Z]: [[0,0],[1,0],[0,1],[-1,1]].map(([x,y]) => ({ x, y }))
+    } as const
+const TetriminoColors : Record<TetriminoTypes, string> = 
+    { [TetriminoTypes.I]: 'red'
+    , [TetriminoTypes.J]: 'blue'
+    , [TetriminoTypes.L]: 'green'
+    , [TetriminoTypes.O]: 'purple'
+    , [TetriminoTypes.S]: 'orange'
+    , [TetriminoTypes.T]: 'gold'
+    , [TetriminoTypes.Z]: 'teal'
+    } as const
+const newBlockGrid = () => [...Array(H)].map(_ => [...Array(W)]) as (string | null)[][]
+const game =
+    { state: STATES.standby
+    , pressing: PRESSING.none
+    , currentBlock: randomBlock()
+    , upcomingBlock: randomBlock()
+    , lastTick: 0
+    , pixelSize: 0
+    , blockGrid: newBlockGrid()
+    , score: 0
+    }
+
+updateDimensions()
+window.addEventListener('resize', updateDimensions)
+function updateDimensions() {
+    const [scaleW, scaleH] = [0.6, 1]
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    game.pixelSize = Math.min(canvas.width * scaleW / W, canvas.height * scaleH / H)
+    ctx.font = '30px Arial'
+    render()
+}
+
+function render() {
+    const functions =
+        { [STATES.standby]:  standby
+        , [STATES.game]:     tetris
+        , [STATES.gameOver]: gameOver 
+        }
+    functions[game.state]()
+}
+
+
+
+
+document.onkeydown = e => {
+    if (game.state !== STATES.game)
+    {
+        const stateMap =
+            { [STATES.standby] : STATES.game
+            , [STATES.gameOver] : STATES.standby
+            }
+        game.state = stateMap[game.state]
+        requestAnimationFrame(render)
+        return
+    }
+    const key = e.key.toLowerCase() as keyof typeof KeyMap
+    if (!KeyMap[key]) return
+    game.pressing |= PRESSING[KeyMap[key]]
+    controlCurrentBlock()
+}
+
+document.onkeyup = e => {
+    const key = e.key.toLowerCase() as keyof typeof KeyMap
+    if (!KeyMap[key]) return
+    game.pressing &= ~PRESSING[KeyMap[key]]
+}
+
+
+
+
+function standby() {
+    const text = 'PRESS ANY KEY TO PLAY'
+    const text2 = 'Keys: W, A, S, D, Q, E'
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = 'white'
+    ctx.fillText(text, canvas.width / 8, canvas.height * .4)
+    ctx.fillText(text2, canvas.width / 8, canvas.height * .6)
+}
+
+function tetris() {
+    const t = Date.now()
+    game.lastTick || (game.lastTick = t)
+
+    tickCurrentBlock(t)
+    clearScreen()
+    drawScore()
+    drawBlocks()
+    requestAnimationFrame(render)
+}
+
+function gameOver() {
+    ctx.fillStyle = 'rgba(0,0,0,0.5)'
+    ctx.beginPath()
+    ctx.fillRect(0,0,canvas.width,canvas.height)
+    ctx.fill()
+    ctx.fillStyle = 'red'
+    ctx.fillText("GAME OVER", canvas.width / 8, canvas.height * .4);
+    ctx.fillStyle = 'white'
+    ctx.fillText("press any key to return", canvas.width / 8, canvas.height * .5);
+    ctx.fillText("to the main menu", canvas.width / 8, canvas.height * .6);
+
+    game.blockGrid = newBlockGrid()
+    game.score = 0
+}
+
+function tickCurrentBlock(t : number) {
+    const dt = t - game.lastTick
+    if (dt >= TICK)
+    {
+        game.lastTick = t
+        game.currentBlock.y++
+        if (collisionExists())
+        {
+            game.currentBlock.y--
+            nextBlock()
+        }
+    }
+}
+
+function clearScreen() {
+    ctx.fillStyle = 'black'
+    ctx.beginPath()
+    ctx.rect(0, 0, canvas.width, canvas.height)
+    ctx.fill()
+    ctx.closePath()
+    ctx.clearRect(0, 0, W * game.pixelSize, H * game.pixelSize)
+}
+
+function drawScore() {
+    ctx.fillStyle = 'white'
+    ctx.fillText(`Score: ${game.score}`, W * game.pixelSize + 10, 40)
+}
+
+function drawBlocks() {
+    ctx.strokeStyle = 'white'
+    drawBlock(game.currentBlock)
+    drawUpcomingBlock(game.upcomingBlock)
+    for (let r = 0; r < H; r++)
+    {
+        for (let c = 0; c < W; c++)
+        {
+            const color = game.blockGrid[r][c]
+            if (color)
+            {
+                ctx.fillStyle = color
+                pixel(c, r)
+            }
+        }
+    }
+}
+
+function drawBlock(b : Tetrimino) {
+    ctx.fillStyle = TetriminoColors[b.type]
+    b.points.forEach(({ x, y }) => pixel(b.x + x, b.y + y))
+}
+
+function drawUpcomingBlock(b : Tetrimino) {
+    const dx = (2 + W) * game.pixelSize
+    const dy =  2 * game.pixelSize
+    ctx.translate(dx, dy)
+    ctx.fillStyle = TetriminoColors[b.type]
+    b.points.forEach(({ x, y }) => pixel(x, y))
+    ctx.translate(-dx, -dy)
+}
+
+function collisionExists() {
+    const xs = game.currentBlock.points.map(({ x }) => x)
+    const ys = game.currentBlock.points.map(({ y }) => y)
+    const { x, y } = game.currentBlock
+    const minX = Math.min(...xs) + x
+    const maxX = Math.max(...xs) + x
+    const maxY = Math.max(...ys) + y
+    
+    if (minX < 0 || maxX >= W || maxY >= H)
+    {
+        return true
+    }
+    return game.currentBlock.points.some(p => {
+        const row = game.blockGrid[y + p.y]
+        return row && row[x + p.x] != null
+    })
+}
+
+function nextBlock() {
+    const { x, y, type } = game.currentBlock
+    if (y < 0)
+    {
+        game.state = STATES.gameOver
+        requestAnimationFrame(render)
+        return
+    }
+    const color = TetriminoColors[type]
+    game.currentBlock.points.forEach(p =>
+        game.blockGrid[y + p.y] &&
+        (game.blockGrid[y + p.y][x + p.x] = color))
+    game.currentBlock = game.upcomingBlock
+    game.upcomingBlock = randomBlock()
+    clearCompletedLines()
+}
+
+function controlCurrentBlock() {
+    const block = game.currentBlock
+    const oldX = block.x
+    const oldY = block.y
+    const oldPoints = block.points
+
+    game.pressing & PRESSING["rotate-left"] && rotateCurrentBlock(true)
+    game.pressing & PRESSING["rotate-right"] && rotateCurrentBlock(false)
+    game.pressing & PRESSING.left && block.x--
+    game.pressing & PRESSING.right && block.x++
+    game.pressing & PRESSING.down && block.y++
+
+    if (game.pressing & PRESSING.up)
+    {
+        while (!collisionExists())
+        {
+            block.y++
+        }
+        block.y--
+    }
+    
+    if (collisionExists())
+    {
+        block.x = oldX
+        block.y = oldY
+        block.points = oldPoints
+    }
+}
+
+function rotateCurrentBlock(clockwise : boolean) {
+    const b = game.currentBlock
+    const transform = (x : number, y : number) => clockwise
+        ? { x: y, y: -x }
+        : { x: -y, y: x }
+    const newPoints = b.points.map(({ x, y }) => transform(x, y))
+    b.points = newPoints
+}
+
+function randomBlock() : Tetrimino {
+    const types = Object.keys(TetriminoShapes) as unknown as TetriminoTypes[]
+    const type = types[Math.random() * types.length | 0]
+    return (
+        { x: W / 2 | 0
+        , y: -2
+        , type
+        , points: TetriminoShapes[type]
+        })
+}
+
+function clearCompletedLines() {
+    const lines = new Set<number>()
+    for (let r = 0; r < H; r++)
+    {
+        if (game.blockGrid[r].every(x => x))
+        {
+            lines.add(r)
+        }
+    }
+    if (!lines.size) return
+
+    game.blockGrid = [...Array(lines.size)].map(_ => [...Array(W)])
+        .concat(game.blockGrid.filter((_,r) => !lines.has(r)))
+
+    game.score += lines.size * W * 2 // Each tile is worth two points
+}
+
+function pixel(x : number, y : number) {
+    const s = game.pixelSize
+    ctx.beginPath()
+    ctx.rect(x * s, y * s, s, s)
+    ctx.fill()
+    ctx.stroke()
+    ctx.closePath()
+}
+
+export {}
